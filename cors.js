ff1b4f312291ad50f3e346c9da015763a78f3d29
trxxxx4 <mishustin.b@gmail.com>
2024-05-28 19:40:26 +0500

    cors updata

--- cors.js
+++ cors.js
@@ -1,24 +1,20 @@
 
-
-const allowedCors = [
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://liim38group.nomorepartiesco.ru",
     'http://localhost:3000',
-    'http://localhost:3001',
-]
+    'http://localhost:3001'
+];
 
+function cors(req, res, next) {
+  const { origin } = req.headers;
 
-
-function cors(req, res, next){
-    const {origin} = req.headers;
-    /*if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin)
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-        res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization')
-      }*/
-      res.header('Access-Control-Allow-Origin', origin)
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-        res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization')
-      
-    next();
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-module.exports = cors;
+module.exports = cors; 
